02b2c2c58c841c997e3ce22d64d4e787ba63cf4a
melody16843 <75027048+melody16843@users.noreply.github.com>
2022-12-26 22:48:37 +0800

    Update useChat.js

--- frontend/src/containers/hooks/useChat.js
+++ frontend/src/containers/hooks/useChat.js
@@ -16,7 +16,7 @@ const savedMe = localStorage.getItem(LOCALSTORAGE_KEY);
 
 const API_ROOT =
   process.env.NODE_ENV === "production"
-    ? 'https://deploy-test-production-b5af.up.railway.app/'
+    ? 'wss://deploy-test-production-b5af.up.railway.app/'
     : 'ws://localhost:4000';
 
 const client = new WebSocket(API_ROOT)
