e3f8a5c96ca3bde6377b46dae2d21cccc33960a8
melody16843 <75027048+melody16843@users.noreply.github.com>
2022-12-26 18:42:49 +0800

    Add files via upload

---
+++ backend/src/server.js
@@ -0,0 +1,57 @@
+import express from 'express'
+import http from 'http'
+import dotenv from "dotenv-defaults"
+import mongoose from 'mongoose'
+import WebSocket from 'ws';
+import wsConnect from './wsConnect'
+import {v4 as uuidv4} from 'uuid'
+// import {useState} from 'react'
+
+import mongo from './mongo'
+
+import path from "path";
+
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+if (process.env.NODE_ENV === "production") {
+  const __dirname = path.resolve();
+  app.use(express.static(path.join(__dirname, "../frontend", "build")));
+  app.get("/*", function (req, res) {
+    res.sendFile(path.join(__dirname, "../frontend", "build", "index.html"));
+  });
+}
+if (process.env.NODE_ENV === "development") {
+	app.use(cors());
+}
+
+mongo.connect();
+
+const server = http.createServer(app)
+const wss = new WebSocket.Server({ server })
+const db = mongoose.connection
+
+
+db.once('open', () => {
+    console.log('mongodb connected')
+    wss.on('connection', (ws) => {
+        ws.id = uuidv4();
+        ws.box = '';
+        // wsConnect.initData(ws)
+        // console.log(ws)
+        ws.onmessage = wsConnect.onMessage(wss, ws);
+        ws.once('close', () => {
+            wsConnect.close(ws);
+        })
+    })
+    // wss.on('close', (ws) => {
+    //     wsConnect.close(ws);
+    // })
+})
+
+const PORT = process.env.PORT ||4000
+server.listen(PORT, () =>{
+    console.log(`app listening on port ${PORT}!`)
+})
